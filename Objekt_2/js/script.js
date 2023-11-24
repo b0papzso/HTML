d0af9cb02cb8377566e8b6c36fae17b9cfdc2ffe
2020b-Papp Zsolt <98013425+b0papzso@users.noreply.github.com>
2023-11-24 09:06:37 +0100

    Add files via upload

---
+++ Objekt_2/js/script.js
@@ -0,0 +1,11 @@
+var person = {firstName: "John", lastName: "Smith"};
+
+console.log(person);
+
+/*person.firstName = "Paul";
+console.log(person);
+delete person.firstName;
+console.log(person);*/
+
+console.log(typeof person.firstName !== "undefined");
+console.log(typeof person.firstName == "John" );
