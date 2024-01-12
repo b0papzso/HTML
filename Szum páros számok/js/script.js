93f8ece8b9bccafd224555345d22e59972c0c8c2
2020b-Papp Zsolt <98013425+b0papzso@users.noreply.github.com>
2024-01-12 13:02:54 +0100

    Add files via upload

---
+++ Szum páros számok/js/script.js
@@ -0,0 +1,15 @@
+const ossze = () =>{
+    let szamszoveg = document.getElementById("szamok").value.split(',');
+    let szamok = []
+    let szum = 0;
+    for (let index = 0; index < szamszoveg.length; index++) {
+        szamok.push(parseFloat(szamszoveg[index]))
+    }
+
+    for (let index = 0; index < szamok.length; index++) {
+        if (Math.pow(szamok[index], 3) % 2 == 1) {
+            szum += Math.pow(szamok[index], 3);
+        }       
+    }
+    document.getElementById("eredmeny").innerHTML = szum;
+}
