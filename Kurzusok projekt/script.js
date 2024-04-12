9780a7d3c56561abf535c2b2825e9954b41bcfb3
2020b-Papp Zsolt <98013425+b0papzso@users.noreply.github.com>
2024-04-12 10:54:34 +0200

    Add files via upload

---
+++ Kurzusok projekt/script.js
@@ -0,0 +1,11 @@
+const url = "https://vvri.pythonanywhere.com/api/courses";
+
+fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data)
+                data.forEach(element => {
+                    console.log(element)
+            })
+        })
+        .catch(error => console.log("Hiba történt: " + error))
