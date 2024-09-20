6ef52434a25a0d8f2dc261e137eb0f954e678226
2020b-Papp Zsolt <98013425+b0papzso@users.noreply.github.com>
2024-09-20 09:02:46 +0200

    kész project

--- CodeWars Score Project/script.js
+++ CodeWars Score Project/script.js
@@ -1,26 +1,32 @@
 const url = "https://www.codewars.com/api/v1/users/"
 
 function getCodeWarsID() {
+        document.getElementById("eredmeny").innerHTML = "";
+        document.getElementById("nyelvek").innerHTML = "";
         var codeWarsName = document.getElementById("fhnev").value;
         fetch(url + codeWarsName
         ).then(response => response.json())
         .then(data => {
             printBasic(data)
         })
-    };
+        .catch(error =>
+        {
+            alert("Hibás felhasználónév!")
+        })
+};
 
 function printBasic(data)
 {
     var eredmeny = document.getElementById("eredmeny");
-    console.log(data)
-    eredmeny.innerHTML += "<h3>Neve:</h3>" + data.name + 
-                        "<br><h3>Összesített pontszám, rang:</h3>" + data.honor + " pont, " + data.ranks.overall.rank  + " rang"
-                        + "<br><h3>Nyelvek:</h3>" + printLang(data);
+    eredmeny.innerHTML += "<table><tbody><tr><th>Neve</th><th>Felhasználóneve</th><th>Összesített pontszám</th><th>Rang</th></tr>" + 
+                        "<tr><td>" + data.name + "</td><td>" + data.username + "</td><td>" + data.honor + "</td><td>" + data.ranks.overall.name + "</td></tr>"
+                        + "</table><br>" + printLang(data);
 }
 
 function printLang(data)
 {
     var eredmeny = document.getElementById("nyelvek");
+    eredmeny.innerHTML += "<tr><th>Nyelvek</th><th>Rang</th><th>Elért pontok</th><th>Elért rang színe</th></tr>"
     for(var element in data.ranks.languages)
     {   
         let langData = data.ranks.languages[element];
@@ -28,6 +34,6 @@ function printLang(data)
         console.log(data.ranks.languages[element].rank)
         console.log(data.ranks.languages[element].score)
         console.log(data.ranks.languages[element].color)
-        eredmeny.innerHTML += "<div>" + "<h4>"  + element + ":"  + "</h4>" + "Rang:" + langData.rank + "<br>Elért pont :" + langData.score + "<br>Elért szint színe:" +  langData.color + " </div> <br>";
+        eredmeny.innerHTML += "<tr><td>" + element + "</td><td>" + langData.name + "</td><td>" + langData.score + "</td><td>" +  langData.color + "</td></tr><br>";
     }
 }
