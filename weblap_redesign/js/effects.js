dd83ffe2959242cbc67091cc7a5bdbf818eb4404
2020b-Papp Zsolt <98013425+b0papzso@users.noreply.github.com>
2025-03-20 12:19:25 +0100

    Add files via upload

---
+++ weblap_redesign/js/effects.js
@@ -0,0 +1,271 @@
+function tev1(){
+    document.getElementById("gomb1").className = "kg";document.getElementById("tevt1").className = "show";
+    document.getElementById("gomb2").className = "";document.getElementById("tevt2").className = "hide";
+    document.getElementById("gomb3").className = "";document.getElementById("tevt3").className = "hide";
+    document.getElementById("gomb4").className = "";document.getElementById("tevt4").className = "hide";
+    document.getElementById("gomb5").className = "";document.getElementById("tevt5").className = "hide";
+    document.getElementById("gomb6").className = "";document.getElementById("tevt6").className = "hide";
+    document.getElementById("gomb7").className = "";document.getElementById("tevt7").className = "hide";
+    document.getElementById("gomb8").className = "";document.getElementById("tevt8").className = "hide";};
+    function tev2(){
+     document.getElementById("gomb1").className = "";document.getElementById("tevt1").className = "hide";
+    document.getElementById("gomb2").className = "kg";document.getElementById("tevt2").className = "show";
+    document.getElementById("gomb3").className = "";document.getElementById("tevt3").className = "hide";
+    document.getElementById("gomb4").className = "";document.getElementById("tevt4").className = "hide";
+    document.getElementById("gomb5").className = "";document.getElementById("tevt5").className = "hide";
+    document.getElementById("gomb6").className = "";document.getElementById("tevt6").className = "hide";
+    document.getElementById("gomb7").className = "";document.getElementById("tevt7").className = "hide";
+    document.getElementById("gomb8").className = "";document.getElementById("tevt8").className = "hide";};
+    function tev3(){
+    document.getElementById("gomb1").className = "";document.getElementById("tevt1").className = "hide";
+    document.getElementById("gomb2").className = "";document.getElementById("tevt2").className = "hide";
+    document.getElementById("gomb3").className = "kg";document.getElementById("tevt3").className = "show";
+    document.getElementById("gomb4").className = "";document.getElementById("tevt4").className = "hide";
+    document.getElementById("gomb5").className = "";document.getElementById("tevt5").className = "hide";
+    document.getElementById("gomb6").className = "";document.getElementById("tevt6").className = "hide";
+    document.getElementById("gomb7").className = "";document.getElementById("tevt7").className = "hide";
+    document.getElementById("gomb8").className = "";document.getElementById("tevt8").className = "hide";};
+    function tev4(){
+     document.getElementById("gomb1").className = "";document.getElementById("tevt1").className = "hide";
+    document.getElementById("gomb2").className = "";document.getElementById("tevt2").className = "hide";
+    document.getElementById("gomb3").className = "";document.getElementById("tevt3").className = "hide";
+    document.getElementById("gomb4").className = "kg";document.getElementById("tevt4").className = "show";
+    document.getElementById("gomb5").className = "";document.getElementById("tevt5").className = "hide";
+    document.getElementById("gomb6").className = "";document.getElementById("tevt6").className = "hide";
+    document.getElementById("gomb7").className = "";document.getElementById("tevt7").className = "hide";
+    document.getElementById("gomb8").className = "";document.getElementById("tevt8").className = "hide";};
+    function tev5(){
+     document.getElementById("gomb1").className = "";document.getElementById("tevt1").className = "hide";
+    document.getElementById("gomb2").className = "";document.getElementById("tevt2").className = "hide";
+    document.getElementById("gomb3").className = "";document.getElementById("tevt3").className = "hide";
+    document.getElementById("gomb4").className = "";document.getElementById("tevt4").className = "hide";
+    document.getElementById("gomb5").className = "kg";document.getElementById("tevt5").className = "show";
+    document.getElementById("gomb6").className = "";document.getElementById("tevt6").className = "hide";
+    document.getElementById("gomb7").className = "";document.getElementById("tevt7").className = "hide";
+    document.getElementById("gomb8").className = "";document.getElementById("tevt8").className = "hide";};
+    function tev6(){
+     document.getElementById("gomb1").className = "";document.getElementById("tevt1").className = "hide";
+    document.getElementById("gomb2").className = "";document.getElementById("tevt2").className = "hide";
+    document.getElementById("gomb3").className = "";document.getElementById("tevt3").className = "hide";
+    document.getElementById("gomb4").className = "";document.getElementById("tevt4").className = "hide";
+    document.getElementById("gomb5").className = "";document.getElementById("tevt5").className = "hide";
+    document.getElementById("gomb6").className = "kg";document.getElementById("tevt6").className = "show";
+    document.getElementById("gomb7").className = "";document.getElementById("tevt7").className = "hide";
+    document.getElementById("gomb8").className = "";document.getElementById("tevt8").className = "hide";};
+    function tev7(){
+     document.getElementById("gomb1").className = "";document.getElementById("tevt1").className = "hide";
+    document.getElementById("gomb2").className = "";document.getElementById("tevt2").className = "hide";
+    document.getElementById("gomb3").className = "";document.getElementById("tevt3").className = "hide";
+    document.getElementById("gomb4").className = "";document.getElementById("tevt4").className = "hide";
+    document.getElementById("gomb5").className = "";document.getElementById("tevt5").className = "hide";
+    document.getElementById("gomb6").className = "";document.getElementById("tevt6").className = "hide";
+    document.getElementById("gomb7").className = "kg";document.getElementById("tevt7").className = "show";
+    document.getElementById("gomb8").className = "";document.getElementById("tevt8").className = "hide";};
+    function tev8(){
+     document.getElementById("gomb1").className = "";document.getElementById("tevt1").className = "hide";
+    document.getElementById("gomb2").className = "";document.getElementById("tevt2").className = "hide";
+    document.getElementById("gomb3").className = "";document.getElementById("tevt3").className = "hide";
+    document.getElementById("gomb4").className = "";document.getElementById("tevt4").className = "hide";
+    document.getElementById("gomb5").className = "";document.getElementById("tevt5").className = "hide";
+    document.getElementById("gomb6").className = "";document.getElementById("tevt6").className = "hide";
+    document.getElementById("gomb7").className = "";document.getElementById("tevt7").className = "hide";
+    document.getElementById("gomb8").className = "kg";document.getElementById("tevt8").className = "show";};
+    
+    function tan1(){
+    document.getElementById("gomb9").className = "kga";document.getElementById("tant1").className = "show";
+    document.getElementById("gomb10").className = "";document.getElementById("tant2").className = "hide";
+    document.getElementById("gomb11").className = "";document.getElementById("tant3").className = "hide";};
+    function tan2(){
+    document.getElementById("gomb9").className = "";document.getElementById("tant1").className = "hide";
+    document.getElementById("gomb10").className = "kga";document.getElementById("tant2").className = "show";
+    document.getElementById("gomb11").className = "";document.getElementById("tant3").className = "hide";};
+    function tan3(){
+    document.getElementById("gomb9").className = "";document.getElementById("tant1").className = "hide";
+    document.getElementById("gomb10").className = "";document.getElementById("tant2").className = "hide";
+    document.getElementById("gomb11").className = "kga";document.getElementById("tant3").className = "show";};
+    
+    function gep1(){
+    document.getElementById("gomb12").className = "kgb";document.getElementById("gept1").className = "show";
+    document.getElementById("gomb13").className = "";document.getElementById("gept2").className = "hide";
+    document.getElementById("gomb14").className = "";document.getElementById("gept3").className = "hide";
+    document.getElementById("gomb15").className = "";document.getElementById("gept4").className = "hide";
+    document.getElementById("gomb16").className = "";document.getElementById("gept5").className = "hide";
+    document.getElementById("gomb17").className = "";document.getElementById("gept6").className = "hide";};
+    function gep2(){
+    document.getElementById("gomb12").className = "";document.getElementById("gept1").className = "hide";
+    document.getElementById("gomb13").className = "kgb";document.getElementById("gept2").className = "show";
+    document.getElementById("gomb14").className = "";document.getElementById("gept3").className = "hide";
+    document.getElementById("gomb15").className = "";document.getElementById("gept4").className = "hide";
+    document.getElementById("gomb16").className = "";document.getElementById("gept5").className = "hide";
+    document.getElementById("gomb17").className = "";document.getElementById("gept6").className = "hide";};
+    function gep3(){
+    document.getElementById("gomb12").className = "";document.getElementById("gept1").className = "hide";
+    document.getElementById("gomb13").className = "";document.getElementById("gept2").className = "hide";
+    document.getElementById("gomb14").className = "kgb";document.getElementById("gept3").className = "show";
+    document.getElementById("gomb15").className = "";document.getElementById("gept4").className = "hide";
+    document.getElementById("gomb16").className = "";document.getElementById("gept5").className = "hide";
+    document.getElementById("gomb17").className = "";document.getElementById("gept6").className = "hide";};
+    function gep4(){
+    document.getElementById("gomb12").className = "";document.getElementById("gept1").className = "hide";
+    document.getElementById("gomb13").className = "";document.getElementById("gept2").className = "hide";
+    document.getElementById("gomb14").className = "";document.getElementById("gept3").className = "hide";
+    document.getElementById("gomb15").className = "kgb";document.getElementById("gept4").className = "show";
+    document.getElementById("gomb16").className = "";document.getElementById("gept5").className = "hide";
+    document.getElementById("gomb17").className = "";document.getElementById("gept6").className = "hide";};
+    function gep5(){
+    document.getElementById("gomb12").className = "";document.getElementById("gept1").className = "hide";
+    document.getElementById("gomb13").className = "";document.getElementById("gept2").className = "hide";
+    document.getElementById("gomb14").className = "";document.getElementById("gept3").className = "hide";
+    document.getElementById("gomb15").className = "";document.getElementById("gept4").className = "hide";
+    document.getElementById("gomb16").className = "kgb";document.getElementById("gept5").className = "show";
+    document.getElementById("gomb17").className = "";document.getElementById("gept6").className = "hide";};
+    function gep6(){
+    document.getElementById("gomb12").className = "";document.getElementById("gept1").className = "hide";
+    document.getElementById("gomb13").className = "";document.getElementById("gept2").className = "hide";
+    document.getElementById("gomb14").className = "";document.getElementById("gept3").className = "hide";
+    document.getElementById("gomb15").className = "";document.getElementById("gept4").className = "hide";
+    document.getElementById("gomb16").className = "";document.getElementById("gept5").className = "hide";
+    document.getElementById("gomb17").className = "kgb";document.getElementById("gept6").className = "show";};
+    
+    
+    function kap1(){
+    document.getElementById("gomb18").className = "kgb";document.getElementById("kapt1").className = "show";
+    document.getElementById("gomb19").className = "";document.getElementById("kapt2").className = "hide";
+    document.getElementById("gomb20").className = "";document.getElementById("kapt3").className = "hide";
+    document.getElementById("gomb21").className = "";document.getElementById("kapt4").className = "hide";
+    document.getElementById("gomb22").className = "";document.getElementById("kapt5").className = "hide";}
+    function kap2(){
+    document.getElementById("gomb18").className = "";document.getElementById("kapt1").className = "hide";
+    document.getElementById("gomb19").className = "kgb";document.getElementById("kapt2").className = "show";
+    document.getElementById("gomb20").className = "";document.getElementById("kapt3").className = "hide";
+    document.getElementById("gomb21").className = "";document.getElementById("kapt4").className = "hide";
+    document.getElementById("gomb22").className = "";document.getElementById("kapt5").className = "hide";}
+    function kap3(){
+    document.getElementById("gomb18").className = "";document.getElementById("kapt1").className = "hide";
+    document.getElementById("gomb19").className = "";document.getElementById("kapt2").className = "hide";
+    document.getElementById("gomb20").className = "kgb";document.getElementById("kapt3").className = "show";
+    document.getElementById("gomb21").className = "";document.getElementById("kapt4").className = "hide";
+    document.getElementById("gomb22").className = "";document.getElementById("kapt5").className = "hide";}
+    function kap4(){
+    document.getElementById("gomb18").className = "";document.getElementById("kapt1").className = "hide";
+    document.getElementById("gomb19").className = "";document.getElementById("kapt2").className = "hide";
+    document.getElementById("gomb20").className = "";document.getElementById("kapt3").className = "hide";
+    document.getElementById("gomb21").className = "kgb";document.getElementById("kapt4").className = "show";
+    document.getElementById("gomb22").className = "";document.getElementById("kapt5").className = "hide";}
+    function kap5(){
+    document.getElementById("gomb18").className = "";document.getElementById("kapt1").className = "hide";
+    document.getElementById("gomb19").className = "";document.getElementById("kapt2").className = "hide";
+    document.getElementById("gomb20").className = "";document.getElementById("kapt3").className = "hide";
+    document.getElementById("gomb21").className = "";document.getElementById("kapt4").className = "hide";
+    document.getElementById("gomb22").className = "kgb";document.getElementById("kapt5").className = "show";}
+    
+    function mun1(){
+    document.getElementById("munka1").className = "aktualismunka";
+    document.getElementById("munka2").className = "";
+    document.getElementById("munka3").className = "";
+    document.getElementById("munka4").className = "";}
+    function mun2(){
+    document.getElementById("munka1").className = "";
+    document.getElementById("munka2").className = "aktualismunka";
+    document.getElementById("munka3").className = "";
+    document.getElementById("munka4").className = "";}
+    function mun3(){
+    document.getElementById("munka1").className = "";
+    document.getElementById("munka2").className = "";
+    document.getElementById("munka3").className = "aktualismunka";
+    document.getElementById("munka4").className = "";}
+    function mun4(){
+    document.getElementById("munka1").className = "";
+    document.getElementById("munka2").className = "";
+    document.getElementById("munka3").className = "";
+    document.getElementById("munka4").className = "aktualismunka";}
+    function mun5(){
+    document.getElementById("munka1").className = "";
+    document.getElementById("munka2").className = "";
+    document.getElementById("munka3").className = "";
+    document.getElementById("munka4").className = "";
+    document.getElementById("munka5").className = "aktualismunka";}
+    
+    
+    function ref1(){
+    document.getElementById("ref1").className = "aktualisref";
+    document.getElementById("ref2").className = "";
+    document.getElementById("ref3").className = "";
+    document.getElementById("ref4").className = "";
+    /* document.getElementById("am1").className = "aktualisrefmenu";
+    document.getElementById("am2").className = "";
+    document.getElementById("am3").className = "";
+    document.getElementById("am4").className = ""; */
+    document.getElementById("gomb1r").className = "kg";
+    document.getElementById("gomb2r").className = "";
+    document.getElementById("gomb3r").className = "";
+    document.getElementById("gomb4r").className = "";}
+    
+    function ref2(){
+    document.getElementById("ref1").className = "";
+    document.getElementById("ref2").className = "aktualisref";
+    document.getElementById("ref3").className = "";
+    document.getElementById("ref4").className = "";
+    /* document.getElementById("am1").className = "";
+    document.getElementById("am2").className = "aktualisrefmenu";
+    document.getElementById("am3").className = "";
+    document.getElementById("am4").className = "";*/
+    document.getElementById("gomb1r").className = "";
+    document.getElementById("gomb2r").className = "kg";
+    document.getElementById("gomb3r").className = "";
+    document.getElementById("gomb4r").className = "";}
+    
+    function ref3(){
+    document.getElementById("ref1").className = "";
+    document.getElementById("ref2").className = "";
+    document.getElementById("ref3").className = "aktualisref";
+    document.getElementById("ref4").className = "";
+    /*document.getElementById("am1").className = "";
+    document.getElementById("am2").className = "";
+    document.getElementById("am3").className = "aktualisrefmenu";
+    document.getElementById("am4").className = "";*/
+    document.getElementById("gomb1r").className = "";
+    document.getElementById("gomb2r").className = "";
+    document.getElementById("gomb3r").className = "kg";
+    document.getElementById("gomb4r").className = "";
+    }
+    function ref4(){
+    document.getElementById("ref1").className = "";
+    document.getElementById("ref2").className = "";
+    document.getElementById("ref3").className = "";
+    document.getElementById("ref4").className = "aktualisref";
+    /*document.getElementById("am1").className = "";
+    document.getElementById("am2").className = "";
+    document.getElementById("am3").className = "";
+    document.getElementById("am4").className = "aktualisrefmenu";*/
+    document.getElementById("gomb1r").className = "";
+    document.getElementById("gomb2r").className = "";
+    document.getElementById("gomb3r").className = "";
+    document.getElementById("gomb4r").className = "kg";
+    }
+    
+    
+            $(window).scroll(function(){
+            if ($(this).scrollTop() > 500) {
+                document.getElementById("fixmenu").className = "show";
+            } else {
+                document.getElementById("fixmenu").className = "hide";
+            }
+            });
+    
+    
+    
+     $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
